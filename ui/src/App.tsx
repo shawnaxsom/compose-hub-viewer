b08679eb0fac82e84a90ca259884f3d495a79f1a
Shawn <shawn.axsom@docker.com>
2022-07-06 13:41:19 -0700

    Update description

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -67,16 +67,10 @@ export function App() {
 
   return (
     <>
-      <Typography variant="h3">Docker extension demo</Typography>
+      <Typography variant="h3">Compose-Hub-Viewer</Typography>
       <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
-        This is a basic page rendered with MUI, using Docker's theme. Read the
-        MUI documentation to learn more. Using MUI in a conventional way and
-        avoiding custom styling will help make sure your extension continues to
-        look great as Docker's theme evolves.
-      </Typography>
-      <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
-        Pressing the below button will trigger a request to the backend. Its
-        response will appear in the textarea.
+        This extension lists out Compose files you've uploaded, shows the
+        contents, and lists links to mentioned Namespaces and Repositories.
       </Typography>
       <Stack direction="row" alignItems="start" spacing={2} sx={{ mt: 4 }}>
         <Button variant="contained" onClick={composeUp}>
