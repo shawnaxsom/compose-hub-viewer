1410e7a258e07f1be01be2a293df9d5176559d6e
Shawn <shawn.axsom@docker.com>
2022-07-06 11:00:02 -0700

    initial commit

---
+++ vm/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	var socketPath string
+	flag.StringVar(&socketPath, "socket", "/run/guest/volumes-service.sock", "Unix domain socket to listen on")
+	flag.Parse()
+
+	os.RemoveAll(socketPath)
+
+	logrus.New().Infof("Starting listening on %s\n", socketPath)
+	router := echo.New()
+	router.HideBanner = true
+
+	startURL := ""
+
+	ln, err := listen(socketPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	router.Listener = ln
+
+	router.GET("/hello", hello)
+	router.GET("/compose-up", composeUp)
+
+	log.Fatal(router.Start(startURL))
+}
+
+func listen(path string) (net.Listener, error) {
+	return net.Listen("unix", path)
+}
+
+func hello(ctx echo.Context) error {
+	return ctx.JSON(http.StatusOK, HTTPMessageBody{Message: "hello"})
+}
+
+func composeUp(ctx echo.Context) error {
+	return ctx.JSON(http.StatusOK, HTTPMessageBody{
+		Message: `version: '3'
+services:
+  hello_world:
+    image: ubuntu
+    command: [/bin/echo, 'Hello world']`,
+	})
+}
+
+type HTTPMessageBody struct {
+	Message string
+}
